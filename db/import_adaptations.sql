3154784c2d126a2ba15da0ad929c5bc8886bab9b
Michael Derntl <michael.derntl@uni-tuebingen.de>
2018-05-24 16:02:02 +0200

    adapt person fields length

--- db/import_adaptations.sql
+++ db/import_adaptations.sql
@@ -4,7 +4,11 @@ UPDATE pg_attribute SET atttypmod = 20+4
 WHERE (attrelid = 'documents'::regclass or attrelid = 'documents_history'::regclass)
 AND attname = 'signature';
 
-drop table if exists neu cascade; 
+UPDATE pg_attribute SET atttypmod = 500+4
+WHERE (attrelid = 'persons'::regclass or attrelid = 'persons_history'::regclass)
+AND attname in ('forename_translit', 'lastname_translit', 'byname_translit', 'forename_arabic', 'lastname_arabic', 'byname_arabic');
+
+drop table if exists neu cascade;
 CREATE TABLE neu (
     zeile int,
     nr text,
