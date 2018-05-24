599bf5a7673c855494e2e15bbea0a743aaa623e2
Michael Derntl <michael.derntl@uni-tuebingen.de>
2018-05-24 11:16:32 +0200

    db preps

--- db/import_adaptations.sql
+++ db/import_adaptations.sql
@@ -1,1 +1,8 @@
-alter type editing_status add value 'imported';
+alter type edit_history_status add value 'imported';
+
+alter table documents add column import_notes text;
+alter table documents_history add column import_notes text;
+
+UPDATE pg_attribute SET atttypmod = 20+4
+WHERE attrelid = 'documents'::regclass or attrelid = 'documents_history'::regclass
+AND attname = 'signature';
