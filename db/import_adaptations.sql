678cb65e2db399d63ac9b9b5efc506c630e7f7aa
Michael Derntl <michael.derntl@uni-tuebingen.de>
2018-05-24 15:47:22 +0200

    db sql

--- db/import_adaptations.sql
+++ db/import_adaptations.sql
@@ -3,3 +3,16 @@ alter type edit_history_status add value 'imported';
 UPDATE pg_attribute SET atttypmod = 20+4
 WHERE (attrelid = 'documents'::regclass or attrelid = 'documents_history'::regclass)
 AND attname = 'signature';
+
+drop table if exists neu cascade; 
+CREATE TABLE neu (
+    zeile int,
+    nr text,
+    dif text,
+    jahr text,
+    datum text,
+    adressat text,
+    absender text,
+    weitere text,
+    inhalt text
+);
