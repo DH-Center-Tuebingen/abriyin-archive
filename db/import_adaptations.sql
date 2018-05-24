2967ee1460bb594acfab62307f4b7e778e226edc
Michael Derntl <michael.derntl@uni-tuebingen.de>
2018-05-24 15:19:26 +0200

    fix sql update

--- db/import_adaptations.sql
+++ db/import_adaptations.sql
@@ -1,5 +1,5 @@
 alter type edit_history_status add value 'imported';
 
 UPDATE pg_attribute SET atttypmod = 20+4
-WHERE attrelid = 'documents'::regclass or attrelid = 'documents_history'::regclass
+WHERE (attrelid = 'documents'::regclass or attrelid = 'documents_history'::regclass)
 AND attname = 'signature';
